d6f20b1d542a6c7f20b6f28f26c5f827a82a5735
westlia <96121397+westlia@users.noreply.github.com>
2025-07-24 22:46:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone --depth 1 https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
+git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
+sed -i 's/192.168.6.1/10.1.1.1/g' package/base-files/files/bin/config_generate
